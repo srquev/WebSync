f59dc9071ef12e6cd5133a489b55d2d4dd52d97f
sharique <ssharique38@gamil.com>
2023-12-16 23:22:56 +0530

    files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JsEditorComponent } from './js-editor/js-editor.component';
+import { ServicesComponent } from './services/services.component';
+import { OurWorkComponent } from './our-work/our-work.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import EditorJS from '@editorjs/editorjs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JsEditorComponent,
+    ServicesComponent,
+    BlogsComponent,
+    OurWorkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EditorJS
   ],
   providers: [],
   bootstrap: [AppComponent]
